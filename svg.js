2a8e63f5e0124e3ce72d30f1abf3ce7343a2b3a5
WarofStates <mdmus213@gmail.com>
2023-05-18 01:11:01 +0300

    initial commit

---
+++ svg.js
@@ -0,0 +1,2 @@
+document.querySelectorAll('path').forEach(e => e.onclick = () => alert(`ID Региона: ${e.getAttribute('id')}`))
+// document.querySelectorAll('path').forEach(e => e.onclick = () => alert(`ID Региона:`))
